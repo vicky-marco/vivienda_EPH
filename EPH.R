89336150d2c7cd85fcff0f2b8d799bcf0a09acfe
vicky-marco <vicky_marco@hotmail.com>
2021-09-03 12:29:38 -0300

    avances

--- EPH.R
+++ EPH.R
@@ -11,5 +11,19 @@ base_P1 <- base %>%
 
 base_P1 <-as.data.frame(lapply(base_P1, rep, base_P1$PONDERA))
 
+base_P1_1 <- base_P1 %>% 
+  group_by(DECIFR, II7) %>% 
+  summarise(cantidad=n()) %>% 
+  arrange(desc(cantidad))
+
+base_P1_2 <- base_P1 %>% 
+  group_by(II7, DECIFR) %>% 
+  summarise(cantidad=n()) %>% 
+  arrange(desc(cantidad))
+
+library(xlsx)
+
+base_P1_1 <- write.xlsx(base_P1_1, "base_1.xlsx")
+
 
 
